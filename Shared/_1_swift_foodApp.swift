98f428862b6961039fdaa64d148c70d2b2982453
Liu Ziyi <ziyi.liu02@gmail.com>
2023-06-26 16:51:44 +0800

    Initial Commit

---
+++ Shared/_1_swift_foodApp.swift
@@ -0,0 +1,17 @@
+//
+//  _1_swift_foodApp.swift
+//  Shared
+//
+//  Created by Liu Ziyi on 26/6/23.
+//
+
+import SwiftUI
+
+@main
+struct _1_swift_foodApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
