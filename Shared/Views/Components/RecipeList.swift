d3507db9bdc11430c890f19d8014eaf93008f7c4
Liu Ziyi <ziyi.liu02@gmail.com>
2023-06-28 21:19:12 +0800

    - Recipe list

---
+++ Shared/Views/Components/RecipeList.swift
@@ -0,0 +1,41 @@
+//
+//  RecipeList.swift
+//  11-swift-food (iOS)
+//
+//  Created by Liu Ziyi on 28/6/23.
+//
+
+import SwiftUI
+
+struct RecipeList: View {
+    var recipes: [Recipe]
+    
+    var body: some View {
+        VStack {
+            HStack {
+                Text("\(recipes.count)\(recipes.count > 1 ? " recipes" : " recipe")")
+                    .font(.headline)
+                    .fontWeight(.medium)
+                    .opacity(0.7)
+                
+                Spacer()
+            }
+            
+            LazyVGrid(columns: [GridItem(.adaptive(minimum: 160), spacing: 15)], spacing: 15) {
+                ForEach(recipes) { recipe in
+                    RecipeCard(recipe: recipe)
+                }
+            }
+            .padding(.top)
+        }
+        .padding(.horizontal)
+    }
+}
+
+struct RecipeList_Previews: PreviewProvider {
+    static var previews: some View {
+        ScrollView {
+            RecipeList(recipes: Recipe.all)
+        }
+    }
+}
--- Shared/Views/Main/HomeView.swift
+++ Shared/Views/Main/HomeView.swift
@@ -10,10 +10,10 @@ import SwiftUI
 struct HomeView: View {
     var body: some View {
         NavigationView {
-            List(Recipe.all) { recipe in
-                Text(recipe.name)
-                    .navigationTitle("My Recipes")
+            ScrollView {
+                RecipeList(recipes: Recipe.all)
             }
+            .navigationTitle("My Recipes")
         }
         .navigationViewStyle(.stack)
     }
