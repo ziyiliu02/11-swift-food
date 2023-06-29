8e389241a4c6b729204ce2a46435dfb5a0265209
Liu Ziyi <ziyi.liu02@gmail.com>
2023-06-29 15:58:01 +0800

    - Add Recipe Form

---
+++ Shared/Views/Detail/AddRecipeView.swift
@@ -0,0 +1,59 @@
+//
+//  AddRecipeView.swift
+//  11-swift-food (iOS)
+//
+//  Created by Liu Ziyi on 29/6/23.
+//
+
+import SwiftUI
+
+struct AddRecipeView: View {
+    
+    @State private var name: String = ""
+    @State private var selectedCategory: Category = Category.main
+    @State private var description: String = ""
+    @State private var ingredients: String = ""
+    @State private var directions: String = ""
+    
+    var body: some View {
+        
+        NavigationView {
+            Form {
+                Section(header: Text("Name")) {
+                    TextField("Recipe Name", text: $name)
+                }
+                
+                Section(header: Text("Category")) {
+                    Picker("Category", selection: $selectedCategory) {
+                        ForEach(Category.allCases) { category in
+                            Text(category.rawValue)
+                                .tag(category)
+                        }
+                    }
+                    .pickerStyle(.menu)
+                }
+                
+                Section(header: Text("Description")) {
+                    TextEditor(text: $description)
+                }
+                
+                Section(header: Text("Ingredients")) {
+                    TextEditor(text: $ingredients)
+                }
+                
+                Section(header: Text("Directions")) {
+                    TextEditor(text: $directions)
+                }
+            }
+            .navigationTitle("New Recipe")
+            .navigationBarTitleDisplayMode(.inline)
+        }
+        .navigationViewStyle(.stack)
+    }
+}
+
+struct AddRecipeView_Previews: PreviewProvider {
+    static var previews: some View {
+        AddRecipeView()
+    }
+}
